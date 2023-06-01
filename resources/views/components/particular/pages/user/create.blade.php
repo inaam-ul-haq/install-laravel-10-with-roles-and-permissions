511fa2474584d2833c513eb3edf92481b43e0090
inaam ul haq <waleedhaq339@gmail.com>
2023-06-02 01:05:04 +0500

    some extra files are removed

--- resources/views/components/particular/pages/user/create.blade.php
+++
@@ -1,3 +0,0 @@
-<div>
-    <!-- Very little is needed to make a happy life. - Marcus Aurelius -->
-</div>
--- resources/views/components/professional/pages/user/show.blade.php
+++
@@ -1,5 +0,0 @@
-<x-front.card>
-    <p>Name: {{$compoData->name}}</p>
-    <p>Username: {{$compoData->username}}</p>
-    <p>e-Mail: {{$compoData->email}}</p>
-</x-front.card>
